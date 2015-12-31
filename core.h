e55acf49aaa7adbe511c3c5245abf02ed1a2ab86
AlexandrovYaroslav <dogmeat95@gmail.com>
2015-12-31 22:27:49 +0300

    Task deskription in core.h

--- core.h
+++ core.h
@@ -5,7 +5,21 @@
 #include <QString>
 #include <QTime>
 
-class Task {};
+class Task
+{
+public:
+    Task(){}; // Конструктор
+    
+private:
+    Qstring name; // Название задачи
+    QDate start_day; // Дата начала выполнения
+    Qdate end_day; // Дата конца выполнения
+    int priority; // Приоритет задачи
+    int completeness; // Процент выполнения задачи
+    QVector<Task> subtasks; // Массив с подзадачами
+    QTime start_time; // Время начала выполнения
+    QTime end_time; // Время конца выполнения
+};
 
 #endif // CORE_H
 
