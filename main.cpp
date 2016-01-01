01082d9c3c9a55b3358f00891536e0bf45c64966
dmitry997990 <dimma995@gmail.com>
2016-01-01 19:49:33 +0300

    Added core.cpp and core.h, fixed bugs in main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,8 @@
-#include <iostream>
 #include <QCoreApplication>
-
-using namespace std;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    cout << "lol";
+
     return a.exec();
 }
